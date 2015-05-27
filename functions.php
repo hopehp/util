35b4293f40b1c6d7c59293456954502a81412bd2
Shvorak Alexey <dr.emerido@gmail.com>
2015-05-27 11:13:42 +0300

    Added method for each arrays

--- functions.php
+++ functions.php
@@ -18,6 +18,30 @@ function array_get($array, $key, $default = null)
 }
 
 /**
+ * Calls handler for each array item
+ *
+ * @param array    $array
+ * @param callable $handler
+ *
+ * ```php
+ * array_each([1 => 1111, 'two' => 2222], function($key, $val) {
+ *      $key; // 1
+ *      $val; // 1111
+ * })
+ * ```
+ * ```php
+ * array_each([1 => 1111, 'two' => 2222], [$obj, 'addParam']);
+ * ```
+ *
+ * @return void
+ */
+function array_each($array, callable $handler) {
+    foreach ($array as $key => $value) {
+        call_user_func($handler, $key, $value);
+    }
+}
+
+/**
  * Call value if its a callable or return his
  *
  * @param mixed|callable $value
@@ -31,4 +55,18 @@ function active_value($value, $args = [])
         return call_user_func($value, ...$args);
     }
     return $value;
+}
+
+/**
+ * Check if value between min and max values
+ *
+ * @param mixed $value
+ * @param mixed $min
+ * @param mixed $max
+ *
+ * @return bool
+ */
+function in_range($value, $min, $max)
+{
+    return $value >= $min && $value <= $max;
 }
